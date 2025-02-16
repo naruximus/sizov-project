76d3a859c203112ec58fe23cc5e3ef4e33d9e6e2
naruximus <naruximus@icloud.com>
2025-02-16 17:48:07 +0400

    temp test

--- apps/app/next.config.js
+++ apps/app/next.config.js
@@ -12,6 +12,16 @@ const nextConfig = {
     // See: https://github.com/gregberge/svgr
     svgr: false,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: '127.0.0.1',
+        port: '1337',
+        pathname: '/uploads/**',
+      },
+    ],
+  },
 };
 
 const plugins = [
--- apps/app/src/views/actor/actor.tsx
+++ apps/app/src/views/actor/actor.tsx
@@ -1,4 +1,60 @@
+'use client';
+
 import { VideoPlayer } from '@/shared/components';
+import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry';
+import Image from 'next/image';
+
+import { useEffect, useState } from 'react';
+import './actor.css';
+
+const getGallery = async () => {
+  const data = await fetch('http://localhost:1337/api/tests?populate=*');
+  const json = await data.json();
+  console.log(json);
+  return json.data[0].gallery;
+};
+
+const getRandomBinary = () => Math.floor(Math.random() * 2);
+
+export const Gallery = () => {
+  const [gallery, setGallery] = useState([]);
+  useEffect(() => {
+    getGallery().then((data) => {
+      console.log('data', data);
+      setGallery(data);
+    });
+  }, []);
+  return (
+    <ResponsiveMasonry
+      columnsCountBreakPoints={{
+        640: 2,
+        768: 3,
+        1024: 4,
+        1280: 5,
+        1440: 7,
+      }}
+    >
+      <Masonry gutter={'12px'}>
+        {gallery.map((e) => {
+          console.log('e', getRandomBinary());
+          //@ts-expect-error formats property may be undefined
+          const data = e?.formats?.small;
+          return (
+            <Image
+              // className="nth-3:col-span-2"
+              //@ts-expect-error formats property may be undefined
+              key={e.id}
+              width={data?.width}
+              height={data?.height}
+              alt="Image 1"
+              src={`http://127.0.0.1:1337${data?.url}`}
+            />
+          );
+        })}
+      </Masonry>
+    </ResponsiveMasonry>
+  );
+};
 
 export function Actor() {
   return (
@@ -52,6 +108,10 @@ export function Actor() {
           <br />
           [музыка].
         </p>
+      </section>
+
+      <section className="py-4 px-8 md:px-16 lg:px-24">
+        <Gallery />
       </section>
     </main>
   );
